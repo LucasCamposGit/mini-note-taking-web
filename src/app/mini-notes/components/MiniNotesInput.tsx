6f689869e55cd866903f22a9de967ef315eed3db
LucasCamposGit <102190513+LucasCamposGit@users.noreply.github.com>
2025-04-11 21:33:53 +0000

    l

--- src/app/mini-notes/components/MiniNotesInput.tsx
+++ src/app/mini-notes/components/MiniNotesInput.tsx
@@ -1,4 +1,41 @@
+import React, { useReducer } from "react";
+
+enum ACTION {
+  SUBMIT_NOTE = "submit_note",
+  TYPING = "typing"
+}
+
+type DispatchAction  {
+  type: ACTION;
+}
+
+interface State {
+  value: string
+}
+
+function reducer(action: DispatchAction, state: State): State {
+
+  switch (action.type) {
+    case ACTION.TYPING:
+      return state;
+
+    default: 
+      return state;
+  }
+}
+
+
+function handleSubmit(event: React.MouseEvent) {
+
+}
+
+const baseState: State = {
+  value: ""
+}
+
 export default function MiniNotesInput() {
+  const [state, dispatch] = useReducer(reducer, baseState)
+
   return (
     <div className="p-4 rounded-xl note-card mb-4">
       <div className="flex">
@@ -7,6 +44,7 @@ export default function MiniNotesInput() {
             <textarea
               className="w-full p-2 rounded-lg resize-none border-0 bg-transparent text-sm text-white placeholder-transparent focus:outline-none peer"
               placeholder="Write your deepest thought — begin with a whisper"
+              value={state.value}
             ></textarea>
             <div className="absolute top-2 left-2 pointer-events-none text-sm peer-focus:hidden">
               <span className="text-zinc-400">Write your deepest thought</span>{" "}
@@ -15,7 +53,7 @@ export default function MiniNotesInput() {
           </div>
           <div className="flex justify-between items-center mt-2">
             <div className="text-sm text-gray-400">0 / 280</div>
-            <button className="btn-tweet py-1 px-4 rounded-full disabled:opacity-50">
+            <button onClick={handleSubmit} className="btn-tweet py-1 px-4 rounded-full disabled:opacity-50">
               Take note
             </button>
           </div>
