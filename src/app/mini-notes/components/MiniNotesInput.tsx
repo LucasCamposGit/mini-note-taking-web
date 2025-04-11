0c3e61be65c3c2d9382214ac63a03ef9d8434966
LucasCamposGit <102190513+LucasCamposGit@users.noreply.github.com>
2025-04-11 22:00:49 +0000

    input change

--- src/app/mini-notes/components/MiniNotesInput.tsx
+++ src/app/mini-notes/components/MiniNotesInput.tsx
@@ -5,19 +5,24 @@ enum ACTION {
   TYPING = "typing"
 }
 
-type DispatchAction  {
+type DispatchAction  = {
   type: ACTION;
+  payload: any;
 }
 
 interface State {
   value: string
 }
 
-function reducer(action: DispatchAction, state: State): State {
+function reducer(state: State, action: DispatchAction): State {
 
   switch (action.type) {
     case ACTION.TYPING:
-      return state;
+      console.log(action.payload)
+      return {
+        ...state,
+        value: action.payload
+      };
 
     default: 
       return state;
@@ -29,12 +34,18 @@ function handleSubmit(event: React.MouseEvent) {
 
 }
 
+
 const baseState: State = {
   value: ""
 }
 
 export default function MiniNotesInput() {
   const [state, dispatch] = useReducer(reducer, baseState)
+
+  function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    dispatch({type: ACTION.TYPING, payload: event.target.value})
+  }
+  
 
   return (
     <div className="p-4 rounded-xl note-card mb-4">
@@ -44,6 +55,7 @@ export default function MiniNotesInput() {
             <textarea
               className="w-full p-2 rounded-lg resize-none border-0 bg-transparent text-sm text-white placeholder-transparent focus:outline-none peer"
               placeholder="Write your deepest thought — begin with a whisper"
+              onChange={handleChange}
               value={state.value}
             ></textarea>
             <div className="absolute top-2 left-2 pointer-events-none text-sm peer-focus:hidden">
