20d7fa71ee95ea805e5a9c05ca437a6ed2ae0939
LucasCamposGit <lucascpn2501@gmail.com>
2025-04-19 11:47:59 -0300

    refactor: clean up InputCounter and enhance InputField with placeholder functionality

--- src/app/mini-notes2/components/input/InputCounter.tsx
+++ src/app/mini-notes2/components/input/InputCounter.tsx
@@ -5,7 +5,6 @@ import { useMiniNotesContext } from "../../context";
 export default function InputCounter() {
   const { state } = useMiniNotesContext();
 
-
   return (
     <div className="text-sm text-gray-400">
       {state.miniNotes.value.length} / 280
--- src/app/mini-notes2/components/input/InputField.tsx
+++ src/app/mini-notes2/components/input/InputField.tsx
@@ -1,10 +1,18 @@
 "use client";
-import { ChangeEvent } from "react";
+import { ChangeEvent, use, useEffect, useRef } from "react";
 import { useMiniNotesContext } from "../../context";
 import { MINI_NOTES_ACTION } from "@/types/action";
 
 export default function InputField() {
   const { state, dispatch } = useMiniNotesContext();
+  const placeholderRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (placeholderRef.current && state.miniNotes.value.length > 0) {
+      placeholderRef.current.style.opacity = state.miniNotes.value ? "0" : "1";
+    }
+  }
+  , [state.miniNotes.value]);
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     dispatch({
@@ -21,6 +29,7 @@ export default function InputField() {
         value={state.miniNotes.value}
       ></textarea>
       <div
+        ref={placeholderRef}
         className="absolute top-3 left-3 pointer-events-none text-sm transition-opacity duration-200 peer-focus:opacity-0"
       >
         <span className="text-blue-400">Write your deepest thought</span>{" "}
